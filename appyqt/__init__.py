90089cb40fd37324fe554e3e034330ba006f962d
ApplePair111 <inonukerem@gmail.com>
2026-06-06 08:09:14 +0300

    adasdasdasdasdasdasdasdasdasd

--- appyqt/__init__.py
+++ appyqt/__init__.py
@@ -1,15 +1,2 @@
-import importlib
-import sys
-
+"""A collection for QoL tools on Python"""
 print("Welcome to APPYQT! GitHub: https://github.com/ApplePair111/appyqt")
-
-# sys.modules.pop(__name__)  # remove from cache, forces re-run next import
-
-def addTool(path: str):
-    # "functions.math.equations" -> from mylib.functions.math.equations import *
-    module = importlib.import_module(f"mylib.{path}")
-    current = sys.modules[__name__]
-    
-    for attr in dir(module):
-        if not attr.startswith("_"):
-            setattr(current, attr, getattr(module, attr))
