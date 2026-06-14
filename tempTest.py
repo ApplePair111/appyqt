e00272397a475d9add90be82e6ea4c226fa09c08
ApplePair111 <inonukerem@gmail.com>
2026-06-14 09:40:23 +0300

    Some stuff

--- tempTest.py
+++ tempTest.py
@@ -1,4 +1,8 @@
-import appyqt.cryptographyTools as crypto
+from appyqt.asyncTools import *
+import time
 
+def myFunction(arg1, arg2):
+    time.sleep(3)
+    return arg1 + arg2
 
-print(crypto.pwdEncrypt("test", "testtesttest"))
+runFunctionAsync(myFunction, "Add two numbers", 1, 3)
---
+++ tests/test_async.py
@@ -0,0 +1,1 @@
+import appyqt.asyncTools
